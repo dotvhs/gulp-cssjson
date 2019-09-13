ff02e6baa75784ac2898def242097a94a73f5c17
dotvhs <voythas@gmail.com>
2019-09-13 02:59:00 +0200

    Cleanup

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var gutil = require("gulp-util");
 var css2json = require("css-to-json");
 
 module.exports = function() {
-	return es.map(function(file, cb) {
-		file.contents = new Buffer(css2json.fromString(file.contents.toString()));
+	function cssjson(file, callback) {
+		file.contents = new Buffer(
+			JSON.stringify(css2json.fromString(file.contents.toString()))
+		);
 		file.path = gutil.replaceExtension(file.path, ".json");
-		cb(null, file);
-	});
+		callback(null, file);
+	}
+
+	return es.map(cssjson);
 };
